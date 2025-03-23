6bc04e47a540e9fbf57ef5b4275200ca636701ca
James Yao <james@jamesyao.co.uk>
2025-03-23 15:11:01 +0000

    added empty modules controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionsModule } from './transactions/transactions.module';
+import { TaxModule } from './tax/tax.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [TransactionsModule, TaxModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
